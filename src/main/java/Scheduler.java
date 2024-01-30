70698483a3b966599fa2374ce144e647c9de8893
korean5078 <korean5078@gmail.com>
2024-01-30 10:31:20 +0900

    Scheduler 객체에 Member 객체 설정

--- src/main/java/Scheduler.java
+++ src/main/java/Scheduler.java
@@ -1,8 +1,10 @@
 import java.util.Calendar;
+import java.util.List;
 
 public class Scheduler {
     private final Calendar calendar = Calendar.getInstance();
     private final int month;
+    private List<Member> members;
 
     public Scheduler() {
         this.month = calendar.get(Calendar.MONTH) + 1;
@@ -11,6 +13,15 @@ public class Scheduler {
     public Scheduler(int month) {
         this.month = month;
         calendar.set(Calendar.MONTH, month - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+
+    public List<Member> getMembers() {
+        return this.members;
     }
 
     public int getMonth() {
--- src/test/java/SchedulerTest.java
+++ src/test/java/SchedulerTest.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +14,24 @@ public class SchedulerTest {
         Scheduler scheduler2 = new Scheduler(2);
         Assertions.assertThat(scheduler2.getMonth()).isEqualTo(2);
         Assertions.assertThat(scheduler2.getLastDay()).isEqualTo(29);
+    }
+
+    @Test
+    void 스케쥴러_멤버_설정() {
+        List<Member> members = new ArrayList<>();
+        Scheduler scheduler = new Scheduler();
+
+        Member member = new Member("member");
+        member.setHolidays(List.of(1,2,3));
+
+        Member member2 = new Member("member");
+        member2.setHolidays(List.of(4,5,6));
+
+        members.add(member);
+        members.add(member2);
+
+        scheduler.setMembers(members);
+        Assertions.assertThat(member.getHolidays()).isEqualTo(scheduler.getMembers().get(0).getHolidays());
+        Assertions.assertThat(member2.getHolidays()).isEqualTo(scheduler.getMembers().get(1).getHolidays());
     }
 }
